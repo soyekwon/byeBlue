02b1593c767058b5c0909ccec59761ab65414cf5
sujin-create <qsoojinq@naver.com>
2021-08-16 21:41:44 +0900

    merge

--- bye_blue/HomeTraining/migrations/0001_initial.py
+++ bye_blue/HomeTraining/migrations/0001_initial.py
@@ -13,31 +13,31 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='OTT',
+            name='HT',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('genre', models.CharField(max_length=30, verbose_name='장르')),
+                ('part', models.CharField(max_length=30, verbose_name='키워드')),
                 ('title', models.CharField(max_length=30, verbose_name='제목')),
                 ('contents', models.TextField(verbose_name='내용')),
                 ('writer', models.CharField(max_length=30, null=True, verbose_name='작성자')),
                 ('write_date', models.DateTimeField(auto_now_add=True, verbose_name='본문 생성시간')),
-                ('funny', models.CharField(max_length=30, null=True, verbose_name='평가')),
+                ('images', models.ImageField(null=True, upload_to='images')),
             ],
             options={
-                'db_table': 'OTT_board',
+                'db_table': 'HT_board',
             },
         ),
         migrations.CreateModel(
-            name='OTT_COMMENT',
+            name='HT_COMMENT',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('contents', models.TextField(verbose_name='댓글내용')),
                 ('writer', models.CharField(max_length=30, verbose_name='댓글 작성이')),
                 ('write_date', models.DateTimeField(auto_now_add=True, verbose_name='댓글 생성시간')),
-                ('board', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='OTT.ott')),
+                ('board', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='HomeTraining.ht')),
             ],
             options={
-                'db_table': 'OTT_COM_board',
+                'db_table': 'HT_COM_board',
             },
         ),
     ]
